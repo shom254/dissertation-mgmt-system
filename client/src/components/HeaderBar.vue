389c8ba7069818febda9f33c5d02e24fe5dc2140
Shom <shom.wong254@gmail.com>
2024-12-06 16:59:07 +0800

    header bar and login page done

--- client/src/components/HeaderBar.vue
+++ client/src/components/HeaderBar.vue
@@ -17,7 +17,6 @@
 /* border-b-gray-300 border-b-2 fixed top-0 w-full bg-white display z-10 */
 #header {
     border-bottom: 4px solid rgb(209 213 219);
-    
     width: 100%;
     height: auto;
     padding-top: 2px;
@@ -32,11 +31,19 @@
 }
 
 #logout{
+    background-color: var(--blue);
+    border-width: 0;
+    box-shadow: 0 6px 8px -1px rgb(0 0 0 / 0.1), 0 4px 6px -2px rgb(0 0 0 / 0.1);
     margin-left: auto;
     margin-right: 1rem;
-    font-size: 1.5rem;
+    font-size: 1.15rem;
     padding: 0.5rem 1rem;
     height: fit-content;
+}
+
+#logout:hover{
+    background-color: var(--darkblue);
+    cursor: pointer;
 }
 
 </style>
--- client/src/components/HomeView.vue
+++ client/src/components/HomeView.vue
@@ -1,11 +1,87 @@
 <template>
-    <h1> HEllo World!</h1>
+    <div id="background">
+        
+        <main v-if="admin" id="admin">
+            <h1>Dissertation Management System</h1>
+            <h2>Admin Sign In</h2>
+            
+            <input class="inputtext" name="password" type="password"/>
+            <button class="button"> Testing </button>
+
+            <h1>{{ dsdsd }}</h1>
+            <span id="switchuser" @click="admin=!admin">Regular User Login ></span>
+        </main>
+        <main v-else id="user">
+            <h1>Dissertation Management System</h1>
+            <h2>Please Sign In (Student / Teacher)</h2>
+            
+            <input class="inputtext" name="password" type="password"/>
+            <button class="button"> Testing </button>
+
+            <h1>{{ dsdsd }}</h1>
+            <span id="switchadmin" @click="admin=!admin">Admin Login ></span>
+        </main>
+    
+    </div>
 </template>
 
 <script setup>
-
+import { ref } from 'vue';
+const admin = ref(false);
 </script>
 
 <style scoped>
+#background {
+    height: 100%;
+    display:flex;
+    flex-direction: column;
+    width: auto;
+    background-color: rgb(238, 239, 242);
+}
+
+.inputtext {
+    width: 25%;
+}
+
+#switchadmin {
+    font-weight: bold;
+    color: darkblue;
+    margin-top: 10px;
+}
+
+#switchuser {
+    font-weight: bold;
+    color: darkblue;
+}
+
+#switchadmin:hover, #switchuser:hover {
+    text-decoration: underline;
+    cursor: pointer;
+}
+
+h1, h2 {
+    width: fit-content;
+    height: fit-content;
+}
+
+main {
+    height: fit-content;
+    width: 40%;
+    margin: auto;
+    gap: 20px;
+    box-shadow: 0 6px 8px -1px rgb(0 0 0 / 0.1), 0 4px 6px -2px rgb(0 0 0 / 0.1);
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    justify-items: start;
+    align-items: center;
+    padding:10px;
+    padding-bottom: 50px;
+}
+
+#admin {
+    gap: 10px;
+}
 
 </style>
